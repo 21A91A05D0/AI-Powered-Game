17233a6ed86edcec72f01a564ce67a38e28ef797
Praveen Pothan Eathala <97424509+21A91A05D0@users.noreply.github.com>
2025-03-17 02:50:37 +0530

    Update helper.py

--- L4/helper.py
+++ L4/helper.py
@@ -88,7 +88,7 @@ def start_game(main_loop, share=False):
         undo_btn="Undo",
         clear_btn="Clear",
         )
-    demo.launch(share=True, server_name="127.0.0.1", server_port=7000)
+    demo.launch(share=True, server_name="0.0.0.0", server_port=10000)
 
 
 everyone_content_policy = {
